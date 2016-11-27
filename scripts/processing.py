7a6112d97c550cb93816b074975dd86ff54050fd
scollis <scollis.acrf@gmail.com>
2016-11-26 21:00:12 -0600

    INIT: Initial commit
    Added some placeholder scripts plus cluster configuration

---
+++ scripts/processing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+scripts.processsing
+=========================
+top level script for processing a cpol file from command line
+on Argonne LCRC Blues
+
+.. autosummary::
+    :toctree: generated/
+    hello_world
+"""
+
+import processing_code as radar_codes
+
+def hello_world():
+    #placeholder file
+    print('why hello Argonne')
+    radar_codes.hello_world()
+
+if __name__ == "__main__":
+    hello_world()
---
+++ scripts/processing_code.py
@@ -0,0 +1,19 @@
+"""
+scipts.processing_code
+=========================
+Common core processing code across the AGU poster
+
+.. autosummary::
+    :toctree: generated/
+    hello_world
+"""
+
+import pyart
+
+WAVE_VERSION = 'First'
+
+def hello_world():
+    #placeholder file
+    print('why hello ', WAVE_VERSION, ' wave')
+
+
