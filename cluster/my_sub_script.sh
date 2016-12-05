a82a7e3fe7ff04867e900952b0402fc3fe7bb2a9
scollis <scollis.acrf@gmail.com>
2016-12-05 16:49:24 -0600

    WIP: uodatred cluster

--- cluster/my_sub_script.sh
+++ cluster/my_sub_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -l nodes=4:ppn=12
-#PBS -l walltime=00:10:00
+#PBS -l walltime=00:20:00
 #PBS -j oe
 #PBS -V
 #PBS -N IPythonMPI0
 
 #source activate radar35
 export LD_LIBRARY_PATH=/home/scollis/coin/lib/:$LD_LIBRARY_PATH
-ipcluster start --n=12 --profile=mpi0
+ipcluster start --n=48 --profile=mpi0
 
